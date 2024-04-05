7acadf229d23ff4e074fc5c30d0c331643874546
retzam <dretzam@gmail.com>
2024-04-05 12:11:58 +0100

    Update cars_map name

--- ai_models/supervised_learning_models/constants/cars_map.py
+++ ai_models/supervised_learning_models/constants/cars_map.py
@@ -1,12 +1,12 @@
 
 CARS_MAP = {
-   0: 'hyundi', 
-   1: 'volkswagen', 
+   0: 'Hyundai', 
+   1: 'Volkswagen', 
    2: 'BMW', 
-   3: 'skoda', 
-   4: 'ford', 
-   5: 'toyota', 
-   6: 'merc',
-   7: 'vauxhall', 
+   3: 'Skoda', 
+   4: 'Ford', 
+   5: 'Toyota', 
+   6: 'Mercedes',
+   7: 'Vauxhall', 
    8: 'Audi',
 }
