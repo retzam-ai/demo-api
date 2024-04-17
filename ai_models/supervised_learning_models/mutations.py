99714cfc72fd4e17f9ef6d0b38e48cf14e317fc1
retzam <dretzam@gmail.com>
2024-04-17 13:01:45 +0100

    Update supervised learning mutations

--- ai_models/supervised_learning_models/mutations.py
+++ ai_models/supervised_learning_models/mutations.py
@@ -1,13 +1,15 @@
-from ai_models.supervised_learning_models.lib.knn_model import KNNModel
+from ai_models.supervised_learning_models.constants.index import DATASETS, MODELS
+from ai_models.supervised_learning_models.lib.model_loader import ModelLoader
 from ai_models.supervised_learning_models.types import SupervisedLearningPredictionType
 from ai_models.types import ErrorType
 import numpy as np
-from ai_models.supervised_learning_models.constants.cars_map import CARS_MAP
+from ai_models.supervised_learning_models.constants.prediction_maps import CARS_MAP, INJURY_MAP
 import graphene
 
 class SupervisedLearningPredictionMutation(graphene.Mutation):
     class Arguments:
         model = graphene.String(required=True)
+        dataset = graphene.String(required=True)
         input = graphene.List(graphene.Float, required=True)
         
 
@@ -15,13 +17,23 @@ class SupervisedLearningPredictionMutation(graphene.Mutation):
     errors = graphene.Field(ErrorType)
     success = graphene.Boolean()
 
-    def mutate(self, info, model, input):
-        if model == 'knn':
-            knn_model = KNNModel()
-            knn_result = knn_model.predict(np.reshape(input, (1, -1)))
-            manufacturer = CARS_MAP[knn_result[0]]
+    def mutate(self, info, model, dataset, input):
+        if model not in MODELS or dataset not in DATASETS:
+            return SupervisedLearningPredictionMutation(success=False, prediction=SupervisedLearningPredictionType(result={}, errors=ErrorType(message="Unsupported model or dataset")))
             
-            return SupervisedLearningPredictionMutation(success=True, prediction=SupervisedLearningPredictionType(result = {"knn": manufacturer}), errors=None)
+        try:
+            modelLoader = ModelLoader(dataset)
+            result = modelLoader.predict(np.reshape(input, (1, -1)))
+            
+            if dataset == 'cars':
+                prediction = CARS_MAP[result[0]]
+            
+            if dataset == 'injury':
+                prediction = INJURY_MAP[result[0]]
+            
+            return SupervisedLearningPredictionMutation(success=True, prediction=SupervisedLearningPredictionType(result = {"knn": prediction}), errors=None)
+        except:
+            pass
         
-        return SupervisedLearningPredictionMutation(success=True, prediction=SupervisedLearningPredictionType(result={}, errors=None))
+        return SupervisedLearningPredictionMutation(success=False, prediction=SupervisedLearningPredictionType(result={}, errors=ErrorType(message="Something went wrong")))
     
