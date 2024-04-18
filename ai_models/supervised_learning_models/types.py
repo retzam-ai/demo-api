a71cc8f95ef7ce78ecb5bcc73412492ef7d4f4f6
retzam <dretzam@gmail.com>
2024-04-19 00:14:19 +0100

    Update supervised learning types

--- ai_models/supervised_learning_models/types.py
+++ ai_models/supervised_learning_models/types.py
@@ -2,6 +2,7 @@ import graphene
 
 class SupervisedLearningModelsType(graphene.ObjectType):
     knn = graphene.String()
+    naive_bayes = graphene.String()
 
 class SupervisedLearningPredictionType(graphene.ObjectType):
     result = graphene.Field(SupervisedLearningModelsType)
