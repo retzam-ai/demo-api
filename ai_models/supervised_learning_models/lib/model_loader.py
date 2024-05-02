9cd3d65a1d7dd8a4e9eb9bf894f7dbc94b9992a8
retzam <dretzam@gmail.com>
2024-05-02 12:44:12 +0100

    Update model loader add machines dataset

--- ai_models/supervised_learning_models/lib/model_loader.py
+++ ai_models/supervised_learning_models/lib/model_loader.py
@@ -1,7 +1,7 @@
 from joblib import load
 
 from ai_models.supervised_learning_models.constants.index import MODELS, MODELS_SLUGS
-from ai_models.supervised_learning_models.constants.prediction_maps import CARS_MAP, INJURY_MAP
+from ai_models.supervised_learning_models.constants.prediction_maps import CARS_MAP, INJURY_MAP, MACHINES_MAP
 
 class ModelLoader:
     
@@ -29,5 +29,8 @@ class ModelLoader:
                 
             elif self.dataset == 'injury':
                 predictions[model['name']] = INJURY_MAP[prediction[0]]
+            
+            elif self.dataset == 'machines':
+                predictions[model['name']] = MACHINES_MAP[prediction[0]]
                 
         return predictions
