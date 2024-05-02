3175d826b1828e7791d991f540f01a4ddddab576
retzam <dretzam@gmail.com>
2024-05-02 12:43:31 +0100

    Update constants add logistic regression

--- ai_models/supervised_learning_models/constants/index.py
+++ ai_models/supervised_learning_models/constants/index.py
@@ -1,7 +1,8 @@
-MODELS = ['knn', 'naive-bayes']
-DATASETS = ['cars', 'injury']
+MODELS = ['knn', 'naive-bayes', 'logistic-regression']
+DATASETS = ['cars', 'injury', 'machines']
 
 MODELS_SLUGS = {
     'knn': 'knn',
     'naive-bayes': 'naive_bayes',
+    'logistic-regression': 'logistic_regression'
 }
