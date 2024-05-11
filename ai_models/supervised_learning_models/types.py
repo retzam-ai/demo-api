235e128e62515cbc95c55035df28aa807f409fa6
retzam <dretzam@gmail.com>
2024-05-11 10:22:18 +0100

    Update types

--- ai_models/supervised_learning_models/types.py
+++ ai_models/supervised_learning_models/types.py
@@ -4,6 +4,8 @@ class SupervisedLearningModelsType(graphene.ObjectType):
     knn = graphene.String()
     naive_bayes = graphene.String()
     logistic_regression = graphene.String()
+    svm = graphene.String()
+    random_forest = graphene.String()
 
 class SupervisedLearningPredictionType(graphene.ObjectType):
     result = graphene.Field(SupervisedLearningModelsType)
