3ba28878c7e22e50b591b3ebcddd75971a70a4ff
retzam <dretzam@gmail.com>
2024-05-11 10:22:38 +0100

    Update model slug

--- ai_models/supervised_learning_models/constants/index.py
+++ ai_models/supervised_learning_models/constants/index.py
@@ -1,8 +1,10 @@
-MODELS = ['knn', 'naive-bayes', 'logistic-regression']
-DATASETS = ['cars', 'injury', 'machines']
+MODELS = ['knn', 'naive-bayes', 'logistic-regression', 'svm', 'random-forest']
+DATASETS = ['cars', 'injury', 'machines', 'marketing', 'diabetes']
 
 MODELS_SLUGS = {
     'knn': 'knn',
     'naive-bayes': 'naive_bayes',
-    'logistic-regression': 'logistic_regression'
+    'logistic-regression': 'logistic_regression',
+    'svm': 'svm',
+    'random-forest': 'random_forest'
 }
