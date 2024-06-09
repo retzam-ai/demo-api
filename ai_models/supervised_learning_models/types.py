36d36f58d50a3ff61904ea9458a43b67deb93036
retzam <dretzam@gmail.com>
2024-06-10 00:51:28 +0100

    Create regression types

--- ai_models/supervised_learning_models/types.py
+++ ai_models/supervised_learning_models/types.py
@@ -1,11 +1,19 @@
 import graphene
 
-class SupervisedLearningModelsType(graphene.ObjectType):
+class ClassificationModelsType(graphene.ObjectType):
     knn = graphene.String()
     naive_bayes = graphene.String()
     logistic_regression = graphene.String()
     svm = graphene.String()
     random_forest = graphene.String()
 
-class SupervisedLearningPredictionType(graphene.ObjectType):
-    result = graphene.Field(SupervisedLearningModelsType)
+class ClassificationPredictionType(graphene.ObjectType):
+    result = graphene.Field(ClassificationModelsType)
+    
+class RegressionModelsType(graphene.ObjectType):
+    simple_linear_regression = graphene.String()
+    multiple_linear_regression = graphene.String()
+    
+class RegressionPredictionType(graphene.ObjectType):
+    result = graphene.Field(RegressionModelsType)
+   
