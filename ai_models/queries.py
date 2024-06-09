2db05e79e869b1a7db704d5df3d7e3585567afc7
retzam <dretzam@gmail.com>
2024-06-10 00:51:19 +0100

    Update

--- ai_models/queries.py
+++ ai_models/queries.py
@@ -1,15 +1,15 @@
-from ai_models.supervised_learning_models.types import SupervisedLearningPredictionType
+from ai_models.supervised_learning_models.types import ClassificationPredictionType
 import graphene
 
 
 
 class Query(graphene.ObjectType):
     """ Models query """
-    supervised_learning_prediction = graphene.Field(SupervisedLearningPredictionType, model=graphene.String())
+    supervised_learning_prediction = graphene.Field(ClassificationPredictionType, model=graphene.String())
     
     """ Models query resolver """
     def resolve_supervised_learning_prediction(self, info, model):
         
         # Nothing here yet.
-        return SupervisedLearningPredictionType(result = None)
+        return ClassificationPredictionType(result = None)
    
